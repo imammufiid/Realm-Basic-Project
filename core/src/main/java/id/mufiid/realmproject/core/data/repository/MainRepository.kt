f862071efedb0e14466f13821eca2968a1be6772
imufiid <imufiid98@gmail.com>
2022-05-15 19:53:00 +0700

    Created repository

--- core/src/main/java/id/mufiid/realmproject/core/data/repository/MainRepository.kt
+++ core/src/main/java/id/mufiid/realmproject/core/data/repository/MainRepository.kt
@@ -1,6 +1,17 @@
 package id.mufiid.realmproject.core.data.repository
 
+import id.mufiid.realmproject.core.data.source.local.PetLocalDataSource
+import id.mufiid.realmproject.core.domain.model.Pet
 import id.mufiid.realmproject.core.domain.repository.IMainRepository
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 
-class MainRepository: IMainRepository {
+class MainRepository(private val petLocalDataSource: PetLocalDataSource): IMainRepository {
+    override fun getPets(): Flow<List<Pet>> {
+        return flow {
+            emit(petLocalDataSource.retrievePetsToAdopt())
+        }.flowOn(IO)
+    }
 }
--- core/src/main/java/id/mufiid/realmproject/core/domain/repository/IMainRepository.kt
+++ core/src/main/java/id/mufiid/realmproject/core/domain/repository/IMainRepository.kt
@@ -1,4 +1,8 @@
 package id.mufiid.realmproject.core.domain.repository
 
+import id.mufiid.realmproject.core.domain.model.Pet
+import kotlinx.coroutines.flow.Flow
+
 interface IMainRepository {
+    fun getPets(): Flow<List<Pet>>
 }
