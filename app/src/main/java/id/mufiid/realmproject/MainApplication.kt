fa726145bc0b6bafc5ccd847fac5e1ced368ea17
imufiid <imufiid98@gmail.com>
2022-05-15 20:01:26 +0700

    reformat

--- app/src/main/java/id/mufiid/realmproject/MainApplication.kt
+++ app/src/main/java/id/mufiid/realmproject/MainApplication.kt
@@ -4,25 +4,28 @@ import android.app.Application
 import id.mufiid.realmproject.core.di.databaseModule
 import id.mufiid.realmproject.core.di.localDataSourceModule
 import id.mufiid.realmproject.core.di.repositoryModule
+import id.mufiid.realmproject.di.viewModelModules
+import io.realm.Realm
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
 
-class MainApplication: Application() {
+class MainApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
-            startKoin {
-                androidLogger(Level.NONE)
-                androidContext(this@MainApplication)
-                modules(
-                    listOf(
-                        databaseModule,
-                        localDataSourceModule,
-                        repositoryModule
-                    )
+        startKoin {
+            androidLogger(Level.NONE)
+            androidContext(this@MainApplication)
+            modules(
+                listOf(
+                    viewModelModules,
+                    databaseModule,
+                    localDataSourceModule,
+                    repositoryModule
                 )
-            }
+            )
+        }
     }
 }
